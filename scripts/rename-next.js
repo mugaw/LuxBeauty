cf6d77220d44a287e93a8f902049fad4ed36e5f5
mugaw <muhammedgawad@gmail.com>
2026-04-22 14:07:06 +0200

    fix: corrected rename-next.js path and restored fs import

--- scripts/rename-next.js
+++ scripts/rename-next.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const outDir = path.join(__dirname, 'out');
+const outDir = path.join(__dirname, '../out');
 
 function getAllFiles(dirPath, arrayOfFiles) {
   const files = fs.readdirSync(dirPath);
